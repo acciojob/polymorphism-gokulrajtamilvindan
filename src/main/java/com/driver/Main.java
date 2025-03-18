8bcf16ed9122387080d6126ff4fc615aa49c0b0a
gokulraj.tamilvindan github <gokulraj.tamilvindan@gmail.com>
2025-03-18 09:03:59 +0530

    Assignment Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,22 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) {
-        Product p = new Product();
-        System.out.println(p.product(4,5));
-        System.out.println(p.product(4, 5, 6));
-        System.out.println(p.product(4.6, 5.6));
-    }
     public static class Product {
         public int product(int x, int y) {
-            return x+y;
+            return x * y;
         }
         public int product(int x, int y, int z) {
-            return x-y+z;
+            return x * y * z;
         }
         public double product(double x, double y) {
-            return x-y;
+            return x * y;
         }
+    }
+    public static void main(String[] args) {
+        Product p = new Product();
+
+        System.out.println(p.product(4,5));
+        System.out.println(p.product(4, 5, 6));
+        System.out.println(p.product(4.0, 5.0));
     }
 }
